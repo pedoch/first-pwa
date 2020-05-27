ab114ec108ea5529567eba97739589452d6f3cb1
PedroOgheneochuko <pedroogheneochuko@gmail.com>
2020-05-28 00:33:08 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,52 @@
+const main = document.querySelector('main');
+const selector = document.querySelector('#sourceSelector');
+const defaultSource = 'the-washington-post';
+
+window.addEventListener('load', async (e) => {
+  updateNews();
+  await updateSources();
+  selector.value = defaultSource;
+
+  selector.addEventListener('change', (e) => {
+    updateNews(e.target.value);
+  });
+
+  if ('serviceWorker' in navigator) {
+    try {
+      navigator.serviceWorker.register('sw.js');
+      console.log('Service Worker registered');
+    } catch (err) {
+      console.log('Service Worker registeration failed');
+    }
+  }
+});
+
+async function updateSources() {
+  const res = await fetch(`https://newsapi.org/v1/sources`);
+  const json = await res.json();
+
+  selector.innerHTML = json.sources
+    .map((src) => `<option value="${src.id}">${src.name}</option>`)
+    .join('\n');
+}
+
+async function updateNews(source = defaultSource) {
+  const res = await fetch(
+    `https://newsapi.org/v2/top-headlines?sources=${source}&apiKey=94d25c6ff6904bf891b6e8048cde38bf`
+  );
+  const json = await res.json();
+
+  main.innerHTML = json.articles.map(createArticle).join('\n');
+}
+
+function createArticle(article) {
+  return `
+    <div class="article" >
+      <a href=${article.url}>
+        <h2>${article.title}</h2>
+        <img src="${article.urlToImage}" >
+        <p>${article.description}</p>
+      </a>
+    </div>
+  `;
+}
---
+++ sw.js
@@ -0,0 +1,28 @@
+importScripts(
+  './node_modules/workbox-sw/build/importScripts/workbox-sw.dev.v2.1.1.js'
+);
+
+const staticAssets = [
+  '/',
+  '/styles.css',
+  '/app.js',
+  '/fallback.json',
+  '/images/dog.png',
+];
+
+const wb = new WorkboxSW();
+
+wb.precache(staticAssets);
+
+wb.router.registerRoute(
+  'https://newsapi.org/(.*)',
+  wb.strategies.networkFirst()
+);
+wb.router.registerRoute(
+  /.*\.(png|jpg|jpeg|gif)/,
+  wb.strategies.cacheFirst({
+    cacheName: 'news-images',
+    cacheExpiration: { maxEntries: 20, maxAgeSeconds: 12 * 60 * 60 },
+    cacheableResponse: { statuses: [0, 200] },
+  })
+);
